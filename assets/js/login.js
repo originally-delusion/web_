50b709fd558356c18597ed23864d8d147eee6d0a
hyx <1371990626@qq.com>
2022-01-13 11:22:13 +0800

    bigeven

---
+++ assets/js/login.js
@@ -0,0 +1,11 @@
+$(function () {
+  //点击去注册账号的链接
+  $('#link_reg').on('click', function () {
+    $('.login-box').hide()
+    $('.reg-box').show()
+  })
+  $('#link_login').on('click', function () {
+    $('.login-box').show()
+    $('.reg-box').hide()
+  })
+})
