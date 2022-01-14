fd728cde6038fd3cd7ae2f8f8a3f1368e3a8a7ad
hyx <1371990626@qq.com>
2022-01-14 11:40:40 +0800

    index

--- assets/js/index.js
+++ assets/js/index.js
@@ -13,8 +13,35 @@ function getUserInfo() {
       Authorization: localStorage.getItem('token') || ''
     },
     success: function(res) {
-      console.log(res)
+      if(res.status !== 0) {
+        return layui.layer.msg('获取用户信息失败！')
+      }
+          //调用 renderAvatar 渲染用户的头像
+    renderAvatar(res.data)
     }
-
   })
+}
+
+//渲染用户的头像
+function renderAvatar(user) {
+  //1.获取用户的名称
+  let name = user.nickname || user.username
+  //2.设置欢迎的文本
+  $('#welcome').html('欢迎&nbsp;&nbsp;') + name
+  // 3 按需渲染用户的头像
+  if(user.user_pic !== null){
+    //3.1渲染图片头像
+    $('.layui-nav-img')
+      .attr('src', user.user_pic)
+      .show()
+    $('.text-avatar').hide()
+  }else{
+    //3.2渲染文本头像
+    $('.layui-nav-img').hide()
+    var first = name[0].toUpperCase()
+    $('.text-avatar')
+      .html(first)
+      .show()
+  }
+
 }
