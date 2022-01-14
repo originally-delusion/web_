be26d3973303e31c94d6dc13d393271a1d92ea4a
hyx <1371990626@qq.com>
2022-01-14 15:52:56 +0800

    index finish

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,6 +2,19 @@ $(function () {
   getUserInfo()
 })
 
+let layer = layui.layer
+$('.btnLogout').on('click',function () {
+  layer.confirm('确认退出登录', {icon: 3, title: '提示'}, function(index) {
+    //1.清空本地存储中的 token
+    localStorage.removeItem('token') 
+    //2.重新跳转到登录页面
+    location.href = '/login.html'
+
+    //关闭 confirm 询问页
+    layer.close(index)
+  })
+
+})
 
 //获取用户的基本信息
 function getUserInfo() {
@@ -9,9 +22,9 @@ function getUserInfo() {
     method:'GET',
     url:'/my/userinfo',
     //headers 就是请求头配置对象
-    headers:{
-      Authorization: localStorage.getItem('token') || ''
-    },
+    // headers:{
+    //   Authorization: localStorage.getItem('token') || ''
+    // },
     success: function(res) {
       if(res.status !== 0) {
         return layui.layer.msg('获取用户信息失败！')
@@ -19,6 +32,15 @@ function getUserInfo() {
           //调用 renderAvatar 渲染用户的头像
     renderAvatar(res.data)
     }
+
+
+    //不论成功还是失败，最终都会调用函数complete 回调函数 封装到了baseAPI 中
+    // complete: function(res) {
+    //   // console.log(res)
+    //   //在complete 回调函数中， 可以使用 responseJSON 拿到服务器响应回来的数据 
+      
+
+    // }
   })
 }
 
@@ -27,7 +49,7 @@ function renderAvatar(user) {
   //1.获取用户的名称
   let name = user.nickname || user.username
   //2.设置欢迎的文本
-  $('#welcome').html('欢迎&nbsp;&nbsp;') + name
+  $('#welcome').html('欢迎&nbsp;&nbsp;' + name)
   // 3 按需渲染用户的头像
   if(user.user_pic !== null){
     //3.1渲染图片头像
